e6043ac93e327b2e035a2a4b6f3289b927de3da9
Alexander090608 <alexambrocio538@gmail.com>
2026-01-13 06:24:28 +0000

    git add. git commit -m "Add HTML, CSS, JS files to display movie list from JSON"

---
+++ script.js
@@ -0,0 +1,35 @@
+const req = new XMLHttpRequest();
+
+req.onreadystatechange = function() {
+    if (req.readyState === 4 && req.status === 200) {
+        let movies = JSON.parse(req.responseText);
+        console.log(movies[1].year);
+
+        const gridContainer = document.getElementById('grid-container');
+
+        movies.forEach(function(movie) {
+            let card = document.createElement("div");
+            card.classList.add("card");
+
+            let castList = movie.cast.join(', ') 
+            let genresList = movie.genres.join(', ') 
+
+            let textData =
+                "<div class='movie-title'>" + movie.title + "</div>" +
+                "<div class='movie-year'>Released: " + movie.year + "</div>" +
+                "<div class='movie-cast'>Cast: " + castList + "</div>" +
+                "<div class='movie-genres'>Genres: " + genresList + "</div>";
+
+            card.innerHTML = textData;
+
+            if (movie.thumbnail) {
+                card.style.backgroundImage = "url(" + movie.thumbnail + ")";
+            }
+
+            gridContainer.appendChild(card);
+        });
+    }
+}
+
+req.open("GET", "movieList.json", true);
+req.send();
